6e316f575533e91e5cdec29abe117a771e5cb782
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-05 18:08:23 +0100

    exercise 3: added return comment

--- src/exercises/exercise-3-area-of-triangle.ts
+++ src/exercises/exercise-3-area-of-triangle.ts
@@ -2,6 +2,7 @@
  * Calculates the area of a triangle by taking base width of the triangle times the height and dividing it by 2
  * @param base - Width of the base of the triangle
  * @param height - Height of the triangle
+ * @returns Area of the triangle defined by base and height
  */
 export const areaOfTriangle = (base: number, height: number): number => {
   return (base * height) / 2
