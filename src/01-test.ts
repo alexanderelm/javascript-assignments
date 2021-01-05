1d438cb97bd31a248b639e6ba4a7133248e251ff
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-05 14:48:37 +0100

    cleanup ready to work

--- src/01-test.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Function that sum two numbers
- * @param a - First number
- * @param b - Second number
- * @returns the summed numbers
- */
-export const sumNumbers = (a: number, b: number): number => {
-  return a + b
-}
