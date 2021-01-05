c83e8d92175453aa1a743d246957087b18ed9c21
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-05 17:33:23 +0100

    exercise 2: corrected comment

--- src/exercises/exercise-2-next-number.ts
+++ src/exercises/exercise-2-next-number.ts
@@ -1,6 +1,6 @@
 /**
- * Function takes a number and adds a value of 1
- * @param initialNumber - input number set to be plus 1
+ * Function takes a number and increments it by 1
+ * @param initialNumber - The initial number
  * @returns initialNumber plus 1
  */
 export const nextNumber = (initialNumber: number): number => initialNumber + 1
