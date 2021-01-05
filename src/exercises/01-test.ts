280e870951eeda0f76ec02693658fb00b1c82a2f
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-05 16:01:13 +0100

    01-test done -with documentation

--- src/exercises/01-test.ts
+++ src/exercises/01-test.ts
@@ -1,3 +1,9 @@
+/**
+ * function that converts minutes to seconds
+ * @param minutes
+ * @returns minutes input times 60
+ */
+
 export var minToSec = (minutes: number): number => {
   return minutes * 60
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { minToSec } from './exercises/01-test'
 
+/**
+ * converts 5 minutes to seconds in console
+ */
 console.log(minToSec(5))
