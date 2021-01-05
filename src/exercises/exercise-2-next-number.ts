7c2599d705e7f28f55e74fc70ed63d56d68a6be9
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-05 17:21:48 +0100

    exercise 2: solved exercise

---
+++ src/exercises/exercise-2-next-number.ts
@@ -0,0 +1,6 @@
+/**
+ *Function takes a number and adds a value of 1
+ * @param initialNumber - input number set to be plus 1
+ * @returns initialNumber plus 1
+ */
+export const nextNumber = (initialNumber: number): number => initialNumber + 1
