53756fb8bc5baa8a247d70e4abb6b641cd9e7134
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-06 18:37:59 +0100

    exercise 9: solved

---
+++ src/exercises/exercise-9-less-or-equal-zero.ts
@@ -0,0 +1,8 @@
+/**
+ * This function checks if the parameter zeroCheck is less or equal to 0
+ * @param zeroCheck - The value that's checked weather it's less, equal or more than 0
+ * @returns True if zeroCheck is less or equal 0 and false if its a valuer higher than 0
+ */
+export const lessOrEqual = (zeroCheck: number): boolean => {
+  return zeroCheck <= 0;
+}
