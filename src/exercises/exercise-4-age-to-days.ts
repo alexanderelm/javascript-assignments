cab5ac994c9658c4dcaf049038962ec76e607fa2
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-06 12:32:41 +0100

    exercise 4: solved

--- src/exercises/exercise-4-age-to-days.ts
+++ src/exercises/exercise-4-age-to-days.ts
@@ -1,1 +1,8 @@
-export {}
+/**
+ * This function takes an age set in years and converts it to days with the formula (x * 365) where x is the age in years
+ * @param ageInYears The desired age in years
+ * @returns The age set in the ageInYears parameter * 365
+ */
+export const ageToDays = (ageInYears: number): number => {
+  return ageInYears * 365
+}
