2813775746f7428556a93799b3139965fbc1d8e2
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-06 16:31:30 +0100

    exercise 7: fixed typo

--- src/exercises/exercise-7-hours-to-seconds.ts
+++ src/exercises/exercise-7-hours-to-seconds.ts
@@ -1,5 +1,5 @@
 /**
- * Function that takes an amount of hours and converts it to seconds by taking (hours * 3200)
+ * Function that takes an amount of hours and converts it to seconds by taking (hours * 3600)
  * @param hours - Amount of hours that is to be converted to seconds
  * @returns Hours in seconds
  */
