9e0f8fdc0aeac0512fedae01c91f34cb8c3b21e2
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-06 16:04:29 +0100

    exercise 7: solved

---
+++ src/exercises/exercise-7-hours-to-seconds.ts
@@ -0,0 +1,8 @@
+/**
+ * Function that takes an amount of hours and converts it to seconds by taking (hours * 3200)
+ * @param hours - Amount of hours that is to be converted to seconds
+ * @returns Hours in seconds
+ */
+export const hoursInSeconds = (hours: number): number => {
+  return hours * 3600
+}
