acd22570c35ff4419229963ad1aa87cc456e3dc7
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-07 16:58:07 +0100

    exercise 14: created Drinks array and Drink type

---
+++ src/exercises/exercise-15-drinks-sorting.ts
@@ -0,0 +1,13 @@
+interface Drink {
+  name: string
+  price: number
+}
+
+const testDrinks: Drink[] = [
+  { name: 'lemonade', price: 50 },
+  { name: 'lime', price: 100 },
+  { name: 'lime', price: 22 },
+  { name: 'lime', price: 12 },
+  { name: 'lime', price: 52 },
+  { name: 'lime', price: 10 },
+]
