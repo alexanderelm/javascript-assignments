5a9f5d7365d317cdf9fd8a89cc2eb2a2a3a4359c
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-07 11:52:00 +0100

    fix: minor corrections

--- src/index.ts
+++ src/index.ts
@@ -6,57 +6,75 @@ import { calculatePower } from './exercises/exercise-5-power-calculator'
 import { findFirstNumber } from './exercises/exercise-6-first-element-in-array'
 import { hoursInSeconds } from './exercises/exercise-7-hours-to-seconds'
 import { remainder } from './exercises/exercise-8-remainder'
-import {lessOrEqual} from './exercises/exercise-9-less-or-equal-zero'
+import { lessOrEqual } from './exercises/exercise-9-less-or-equal-zero'
+import { andOperator } from './exercises/exercise-11-using-and-operator'
 
 /**
  * converts 5 minutes to seconds in console
  */
-console.log('Exercise 1')
+console.group('Exercise 1')
 console.log(minutesToSeconds(8))
+console.groupEnd()
 
 /**
  * Takes the initialNumber and adds a value of 1, converting -3 to -2
  */
-console.log('Exercise 2')
+console.group('Exercise 2')
 console.log(nextNumber(-3))
+console.groupEnd()
 
 /**
  * Calculates area of a triangle from a base and height parameter
  */
-console.log('Exercise 3')
+console.group('Exercise 3')
 console.log(areaOfTriangle(10, 10))
+console.groupEnd()
 
 /**
  * Converts and amount of years to days
  */
-console.log('Exercise 4')
+console.group('Exercise 4')
 console.log(ageToDays(26))
+console.groupEnd()
 
 /**
  * Calculates power from voltage and current
  */
-console.log('Exercise 5')
+console.group('Exercise 5')
 console.log(calculatePower(230, 20))
+console.groupEnd()
 
 /**
  * Takes an array and returns the first number of the array
  */
-console.log('Exercise 6')
+console.group('Exercise 6')
 console.log(findFirstNumber([4, 3, 8, 9]))
+console.groupEnd()
 
 /**
  * Converts hours to seconds
  */
-console.log('Exercise 7')
+console.group('Exercise 7')
 console.log(hoursInSeconds(5))
+console.groupEnd()
 
 /**
  * Finds the remainder of a division with 2 numbers
  */
-console.log('Exercise 8')
+console.group('Exercise 8')
 console.log(remainder(-9, 45))
+console.groupEnd()
 
 /**
  * checks is the input is less, equal or more than 0
  */
+console.group('Exercise 9')
 console.log(lessOrEqual(5))
+console.groupEnd()
+
+/**
+ * Checks if both first and seconds argument is true and returns true if it's the case, else it will return false
+ */
+console.group('Exercise 11')
+console.log(andOperator(true, false))
+console.groupEnd()
