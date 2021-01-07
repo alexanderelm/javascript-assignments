3491b075fa2270c0e591fe182339d03f9a05d711
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-07 13:29:05 +0100

    exercise 12: added comment to function

--- src/exercises/exercise-12-equal-numbers.ts
+++ src/exercises/exercise-12-equal-numbers.ts
@@ -1,3 +1,9 @@
+/**
+ * Function checks the num1 and num2 values and returns true if they're equal
+ * @param num1 - First value
+ * @param num2 - Second value
+ * @returns True if the num1 parameter is equal to the num2 parameter
+ */
 export const equalNumbers = (num1: number, num2: number): boolean => {
   return num1 == num2
 }
