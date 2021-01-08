3cd293dfcd5b2e4be84a76df6997dffa662803a6
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-08 11:26:25 +0100

    exercise 15: solved

--- src/exercises/exercise-15-drinks-sorting.ts
+++ src/exercises/exercise-15-drinks-sorting.ts
@@ -1,13 +1,28 @@
+/**
+ * defining a new type
+ */
 interface Drink {
   name: string
   price: number
 }
 
-const testDrinks: Drink[] = [
+/**
+ * test array of drinks
+ */
+export const testDrinks: Drink[] = [
   { name: 'lemonade', price: 50 },
   { name: 'lime', price: 100 },
-  { name: 'lime', price: 22 },
-  { name: 'lime', price: 12 },
-  { name: 'lime', price: 52 },
-  { name: 'lime', price: 10 },
+  { name: 'cola', price: 22 },
+  { name: 'lemon', price: 12 },
+  { name: 'sprite', price: 52 },
+  { name: 'squash', price: 10 },
 ]
+
+/**
+ * Function that receives an array and sorts it by price low to high, either by getting an array variable or getting specified an array in the Drink[] type format
+ * @param drinkArray - Receives an array in the Drink[] type format
+ * @returns The input array sorted by price, low to high
+ */
+export const sortDrinkByPrice = (drinkArray: Drink[]) => {
+  return drinkArray.sort((a, b) => a.price - b.price)
+}
