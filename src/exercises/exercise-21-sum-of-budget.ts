7f445f0edc63450c1003e7f162fa4a4fcaab5248
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-11 18:06:36 +0100

    exercise 21: fixed Person interface name

--- src/exercises/exercise-21-sum-of-budget.ts
+++ src/exercises/exercise-21-sum-of-budget.ts
@@ -1,7 +1,7 @@
 /**
  * The person object structure
  */
-interface person {
+interface Person {
   name: string
   age: number
   budget: number
@@ -10,9 +10,9 @@ interface person {
 /**
  * Function that takes an object with name, age and budget and sums up the budget of all the objects
  * @param personInfo - The input containing name, age and budget
- * @returns The budget of the input person objects summed up
+ * @returns The budget of the input Person objects summed up
  */
-export const sumBudgets = (personInfo: person[]): number =>
-  personInfo.reduce((acc: number, current: person) => {
+export const sumBudgets = (personInfo: Person[]): number =>
+  personInfo.reduce((acc: number, current: Person) => {
     return acc + current.budget
   }, 0)
