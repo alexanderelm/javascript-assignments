f7f447c206e15aaec5626b64be8072744fef0ec1
Alexander Andersen <alexander.andersen@adaptagency.com>
2021-01-13 16:06:02 +0100

    exercise 26: solved

---
+++ src/exercises/exercise-26-seven-boom.ts
@@ -0,0 +1,10 @@
+/**
+ * Function that takes an array of numbers, converts it to a string and splits it up on single numbers, then checks if 7 is included on the string
+ * @param numbers - The input array of numbers
+ * @returns A string dependant on weather splitToDigits is truthy or falsy
+ */
+export const sevenBoom = (numbers: number[]): string => {
+  const splitToDigits = numbers.toString().split('').map(Number).includes(7)
+
+  return splitToDigits! ? 'boom' : 'There is no 7 in the array'
+}
